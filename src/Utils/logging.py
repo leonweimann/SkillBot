0af2504c9d3c145b41531cc8832700723b2fd905
Leon Weimann <leonweimann@icloud.com>
2025-01-20 20:02:20 +0100

    [Feature] Add logging utility to send messages to the logs channel

---
+++ src/Utils/logging.py
@@ -0,0 +1,11 @@
+from discord import Guild
+
+from channels import get_channel_by_name
+from errors import CodeError
+
+
+async def log(guild: Guild, message: str):
+    logs_channel = get_channel_by_name(guild, "logs")
+    if logs_channel is None:
+        raise CodeError("Logs channel not found")
+    await logs_channel.send(message)
