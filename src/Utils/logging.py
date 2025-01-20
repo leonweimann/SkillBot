686c516eaf9fe117cea9ae82a9cee7c6e9938935
Leon Weimann <leonweimann@icloud.com>
2025-01-20 20:44:33 +0100

    [Feature] Enhance logging function to include detailed message formatting and optional details

--- src/Utils/logging.py
+++ src/Utils/logging.py
@@ -4,8 +4,16 @@ from .channels import get_channel_by_name
 from .errors import CodeError
 
 
-async def log(guild: Guild, message: str):
+async def log(guild: Guild, message: str, details: dict[str, str] = {}):
     logs_channel = get_channel_by_name(guild, "logs")
     if logs_channel is None:
         raise CodeError("Logs channel not found")
-    await logs_channel.send(message)
+
+    res = f'{"-" * 20}\n**[LOG]** {message}\n```'
+    if details:
+        max_key_length = max(len(key) for key in details.keys())
+        for key, value in details.items():
+            res += f'{key}{" " * (max_key_length - len(key))} | {value}\n'
+    res += '```'
+    res += f'\n{"-" * 20}'
+    await logs_channel.send(res)
