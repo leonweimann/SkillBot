4dbe782bafbd73288525dc91bfbe26e67610d08c
Leon Weimann <leonweimann@icloud.com>
2025-02-02 20:30:29 +0100

    [Feature] Implement setup_hook for dynamic loading of cogs on bot startup

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@ import discord
 from discord.ext import commands
 
 from dotenv import load_dotenv
-
 import os
 import asyncio
 
 from Utils.database import *  # Creates tables if required
-
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -17,11 +15,20 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
     print('------')
 
+
+async def setup_hook():
+    for filename in os.listdir('./src/cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
     try:
         synced_commands = await bot.tree.sync()
         print(f'Synced {len(synced_commands)} commands')
     except Exception as e:
         print(f'Failed to sync commands: {e}')
+
+
+bot.setup_hook = setup_hook
 
 
 def get_discord_token() -> str:
@@ -32,15 +39,9 @@ def get_discord_token() -> str:
     return token
 
 
-async def load_cogs():
-    for filename in os.listdir('./src/cogs'):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'cogs.{filename[:-3]}')
-
-
 async def main():
     async with bot:
-        await load_cogs()
         await bot.start(get_discord_token())
+
 
 asyncio.run(main())
